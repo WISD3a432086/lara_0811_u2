e97e415ff792e8f3037e73f67ad7ccc786ac5b99
陳泓維 <tim5556618@gmail.com>
2017-10-26 12:10:49 +0800

    設定 Route 接收參數

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,19 @@
 */
 
 Route::get('/', function () {
+    return 'welcome';
+});
+
+
+Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/', function () {
+    return redirect('welcome');
+});
+
+Route::get('hello/{name}' , function($name) {
+    return 'Hello, '.$name;
 });
